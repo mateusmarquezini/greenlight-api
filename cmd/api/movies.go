2665d116733766bc3657565b9a7a0b68d73529d5
Mateus Marquezini <mateusmarquezini01@gmail.com>
2022-11-19 09:33:07 -0300

    first commit

---
+++ cmd/api/movies.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bitbucket.org/mateusmarquezini/greenlight/internal/data"
+	"bitbucket.org/mateusmarquezini/greenlight/internal/validator"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
+	}
+
+	if err := app.readJSON(w, r, &input); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
+	v := validator.New()
+
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+	}
+
+	fmt.Fprintf(w, "%+v\n", input)
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+
+	if err := app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil); err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,7 @@
+module bitbucket.org/mateusmarquezini/greenlight
+
+go 1.19
+
+require github.com/julienschmidt/httprouter v1.3.0
+
+require github.com/lib/pq v1.10.2
