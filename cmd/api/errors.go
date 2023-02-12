fdb0a6bd8ca898d10f2718985af58d7e0e5ae24c
Mateus Marquezini <mateusmarquezini01@gmail.com>
2023-02-12 14:41:44 -0300

    chapter 13.1

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -44,5 +44,9 @@ func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.
 func (app *application) editConflictResponse(w http.ResponseWriter, r *http.Request) {
 	message := "unable to update the record due to an edit conflict, please try again"
 	app.errorResponse(w, r, http.StatusConflict, message)
+}
 
+func (app *application) rateLimitExceededResponse(w http.ResponseWriter, r *http.Request) {
+	message := "rate limit exceeded"
+	app.errorResponse(w, r, http.StatusTooManyRequests, message)
 }
--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.19
 
 require github.com/julienschmidt/httprouter v1.3.0
 
-require github.com/lib/pq v1.10.2
+require (
+	github.com/lib/pq v1.10.2
+	golang.org/x/time v0.3.0
+)
